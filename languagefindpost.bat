869c9e8c15d71b8230ec0e88cd04ca63686c1665
dushyantfst <dushyant_fst@yahoo.com>
2016-08-25 22:27:00 +0530

    bat file
    bat file

--- languagefindpost.bat
+++ languagefindpost.bat
@@ -1,4 +1,5 @@
-#!/bin/sh
+rem download curl https://curl.haxx.se/download.html
+echo off
  
 echo "Call to 'demo' App having 'language' Table find using POST"
 curl -H "access_token : Zs0nTQB-ujOSV0KmEoPhBx2E6-Ab_GKO" -H "Content-Type : application/json" --data "{ \"filter\" : { \"Languagename\" : \"English\" }}" http://api.masterdatanode.com/demo/language/find/
--- languagelist.bat
+++ languagelist.bat
@@ -1,4 +1,4 @@
-#!/bin/sh
- 
+rem download curl https://curl.haxx.se/download.html
+echo off
 echo "Call to 'demo' App having 'language' Table Listing "
 curl -H "access_token : Zs0nTQB-ujOSV0KmEoPhBx2E6-Ab_GKO" -H "Content-Type : application/json" http://api.masterdatanode.com/demo/language/find/
